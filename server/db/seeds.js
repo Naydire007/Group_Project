7fe007186aa28c0c5193f740b99a529dc4aa872f
Sara <sluisdejesus@gmail.com>
2021-08-07 15:14:02 +0100

    keys added to seeds.js

---
+++ server/db/seeds.js
@@ -0,0 +1,22 @@
+use users;
+
+debugger.userInfo.insertMany([
+    {
+      "Username":
+      "Name":
+      "Diet":
+      "Transport":  
+    },
+    {
+        "Username":
+        "Name":
+        "Diet":
+        "Transport":  
+    },
+    {
+        "Username":
+        "Name":
+        "Diet":
+        "Transport":
+      }
+])
