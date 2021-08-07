c9dccd73cb12a74ce09271cedaa88466e5f5bfff
Stephanie <stephania.l.e@gmail.com>
2021-08-07 17:51:31 +0100

    server.js and folders added

--- server/server.js
+++ server/server.js
@@ -12,22 +12,22 @@ const createRouter = require('./helpers/create_router.js');
 
 MongoClient.connect('mongodb://localhost:27017')
 .then((client)=> {
-    const db = client.db('group_project'); //name of the data base needed (temporarely name added)
-    //add here the equivalent to bookingsCollection and bookingsRouter connection
-
+    const db = client.db('users'); //name of the data base needed (temporarely name added)
+    const UsersCollection = db.collection('userInfo')
+    const C02Router = createRouter(UserCollection);
 
 app.post('/api/C02', (req, res) => {
     const newData = req.body;
-    if (newData.hasOwnProperty("UserName") && newData.hasOwnProperty("email")) {
-            Collection //collection name is needed we need the folders first
-        .insertOne(newData)
-        .then((result) => {
+    if (newData.hasOwnProperty("Username") && newData.hasOwnProperty("Name")) {
+        UsersCollection //collection name is needed we need the folders first
+          .insertOne(newData)
+          .then((result) => {
           res.json(result.ops[0]);
         })
-        .catch((err) => {
-          console.error(err);
-          res.status(500);
-          res.json({ status: 500, error: err });
+          .catch((err) => {
+            console.error(err);
+            res.status(500);
+            res.json({ status: 500, error: err });
         });
     } else {
       res.status(400);  // bad request
@@ -35,11 +35,11 @@ app.post('/api/C02', (req, res) => {
     }
   });
 
-  app.use('/api/groupProject', bookingsRouter);
+  app.use('/api/userInfo', C02Router);
 
 });
 
-.catch(console.error);
+.catch(console.error)
 
 app.listen(5000, function() {
 console.log(`Group Project running on port ${this.address().port}`);
